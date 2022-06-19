c8743d44a15c7c6a0038641dc31f2c0d60fe6df2
cansu <1018710261@qq.com>
2022-06-19 09:20:13 +0800

    Initial commit

---
+++ src/test/java/com/yjc/springbootdrools/SpringBootDroolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yjc.springbootdrools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDroolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
